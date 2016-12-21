d27c3b3f201c51ab4d02c3b0d6d84eb90eb37e63
j2FunOnly <j2funonly@ya.ru>
2016-12-21 10:09:49 +0300

    it works

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -2,11 +2,12 @@ require 'crypto'
 require 'pry'
 
 RSpec.describe Crypto do
+  subject { described_class.new key }
+
   describe 'decode and encode phrase' do
     let(:decoded) { 'Hello,_world!' }
     let(:encoded) { 'lHe,olo_wdlr!' }
     let(:key) { '312654' }
-    subject { described_class.new key }
 
     describe '#encode' do
       it 'return ecoded string' do
@@ -18,6 +19,16 @@ RSpec.describe Crypto do
       it 'return decoded string' do
         expect(subject.decode encoded).to eq decoded
       end
+    end
+  end
+
+  describe 'encode and decode string with random key' do
+    let(:key) { (1..rand(1..9)).to_a.shuffle.join }
+    let(:decoded) { 'Lorem ipsum dolor sit amet' }
+    let(:encoded) { subject.encode decoded }
+
+    it 'successfully' do
+      expect(subject.decode encoded).to eq decoded
     end
   end
 end
