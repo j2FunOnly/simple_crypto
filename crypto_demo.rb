9576f98c2fb9d281c585a895d04138ee7bc0e2c7
j2FunOnly <j2funonly@ya.ru>
2016-12-22 09:13:44 +0300

    Fix demo

--- crypto_demo.rb
+++ crypto_demo.rb
@@ -3,13 +3,13 @@ require_relative 'lib/crypto'
 c = Crypto.new (1..9).to_a.shuffle.join
 
 File.open('tmp/crypto.encoded', 'w') do |target|
-  File.open('crypto/lib/crypto.rb', 'r').each do |src|
+  File.open('lib/crypto.rb', 'r').each do |src|
     target.puts c.encode(src.chomp)
   end
 end
 
 File.open('tmp/crypto.decoded.rb', 'w') do |target|
-  File.open('crypto.encoded', 'r').each do |src|
+  File.open('tmp/crypto.encoded', 'r').each do |src|
     target.puts c.decode(src.chomp)
   end
 end
