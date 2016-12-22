9ca4b173001c53af6df94e12d4598b26d0e59ad5
j2FunOnly <j2funonly@ya.ru>
2016-12-22 09:09:27 +0300

    add tmp and .gitignore

--- crypto_demo.rb
+++ crypto_demo.rb
@@ -2,14 +2,14 @@ require_relative 'lib/crypto'
 
 c = Crypto.new (1..9).to_a.shuffle.join
 
-File.open('crypto.encoded', 'w') do |target|
+File.open('tmp/crypto.encoded', 'w') do |target|
   File.open('crypto/lib/crypto.rb', 'r').each do |src|
     target.puts c.encode(src.chomp)
   end
 end
 
-File.open('crypto.decoded.rb', 'w') do |target|
+File.open('tmp/crypto.decoded.rb', 'w') do |target|
   File.open('crypto.encoded', 'r').each do |src|
-    target.puts c.decode src.chomp
+    target.puts c.decode(src.chomp)
   end
 end
